aedccebd71ec931fcf6df6bc6f7dff71d16f0e4a
Iain MacDonald <IJMacD@gmail.com>
2015-09-18 23:18:15 +0800

    Local storage of tracks

--- js/TrackStore.js
+++ js/TrackStore.js
@@ -1,7 +1,8 @@
 function TrackStore(){
   riot.observable(this)
 
-  this.tracks = []
+  const LOCALSTORAGE_KEY = 'gpstools-track-index'
+  this.tracks = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || []
 
   this.on('track_init', () => {
     this.trigger('track_changed', this.tracks)
@@ -13,16 +14,19 @@ function TrackStore(){
   })
 
   this.on('track_remove', track => {
-    var index = this.tracks.indexOf(track);
-    if(index >= 0){
+    var index = this.tracks.indexOf(track)
+    if(index >= 0)
       this.tracks.splice(index, 1)
-    }
     else
       this.tracks.pop()
-    this.trigger('todo_changed', this.tracks)
+    this.trigger('track_changed', this.tracks)
   })
 
   this.on('track_edit', track => {
     this.trigger('track_changed', this.tracks);
+  })
+
+  this.on('track_changed', tracks => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(this.tracks))
   })
 }
